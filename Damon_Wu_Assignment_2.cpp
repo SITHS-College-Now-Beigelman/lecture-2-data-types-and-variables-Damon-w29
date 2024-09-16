768923259fd14162c66b01a08f2c6f9f9ff8f5cb
Damon-w29 <Damonw18@nycstudents.net>
2024-09-16 16:25:24 -0400

    Create Damon_Wu_Assignment_2.cpp

---
+++ Damon_Wu_Assignment_2.cpp
@@ -0,0 +1,28 @@
+// Damon Wu 
+// Lecture 2 
+// 9/16/2024
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+// Listing the variables 
+    int Inters  = 12;           // Integer (Whole Number)
+    float Stilt = 2.71;         // Floating Point Number
+    double floatsm = 8.28;     // Floating Point number 
+    char Jos = 'H';            // Character
+    bool Boo = true;           // Boolean 
+    string Gpu = "AMD";        // String 
+
+// Slide 37 
+    cout << inters << "inch(es) = " ; //output the value of inches and the equal sign
+    cout << inters / 12 << " feet (foot) and " ; // output the amount of feet 
+    cout << inters 
+
+// Comparisons 
+    cout << "2 + 3.5 = " << 2 + 3.5 << endl;
+
+    
+    return 0;
+}
