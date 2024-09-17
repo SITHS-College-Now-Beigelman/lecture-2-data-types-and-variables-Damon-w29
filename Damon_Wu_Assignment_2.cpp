95b72bb8e39618572d7d48cdf3b2db00c1473ae7
Damon-w29 <Damonw18@nycstudents.net>
2024-09-17 15:41:57 -0400

    Fixed outputs

--- Damon_Wu_Assignment_2.cpp
+++ Damon_Wu_Assignment_2.cpp
@@ -32,7 +32,11 @@ int main() {
     cout << inches % 12 << " inch(es)" << endl;  // output remainding inches
 
 // Comparisons 
-   cout << Jos <<  " < " << 'Q' << "\n";            //Show that the value of Q is higher than the value of "JOS"
+    char bot = 'T'; // List new variable
+    char max = 'a'; // List other new variable
+
+    cout << "T < a = " << (bot < max) << "\n"; //outputs
+
 
 // Expressions 
     cout << "2 + 3.5 = " << 2 + 3.5 << endl;                                 // simple mathematical expression
