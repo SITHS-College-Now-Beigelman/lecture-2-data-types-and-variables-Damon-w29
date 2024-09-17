b9bbef755d553710f631a822adb1daf52076e913
Damon-w29 <Damonw18@nycstudents.net>
2024-09-17 15:33:27 -0400

    updates

--- Damon_Wu_Assignment_2.cpp
+++ Damon_Wu_Assignment_2.cpp
@@ -8,26 +8,36 @@ using namespace std;
 
 int main() {
 // Listing the variables 
-    int Inters  = 12;           // Integer (Whole Number)
-    float Stilt = 2.71;         // Floating Point Number
-    double floatsm = 8.28;     // Floating Point number 
-    char Jos = 'H';            // Character
-    bool Boo = true;           // Boolean 
-    string Gpu = "AMD";        // String 
+    int Inters  = 12;                            // Integer (Whole Number)
+    float Stilt = 2.71;                          // Floating Point Number
+    double floatsm = 8.28;                       // Floating Point number 
+    char Jos = 'H';                              // Character
+    bool Boo = true;                             // Boolean 
+    string Gpu = "AMD";                          // String 
+
+// Listing value of variables 
+    cout << "Int: " << Inters << "\n";
+    cout << "float: " << Stilt << "\n";
+    cout << "double: " << floatsm << "\n";
+    cout << "char: " << Jos << "\n";
+    cout << "bool: " << Boo << "\n";
+    cout << "string: " << Gpu << "\n";
+ 
 
 // Slide 37 
-    int inches; // A variable to store total inches 
-    inches = 120; // Setting the value for inches 
-    cout << inches << "inch(es) = " ; //output the value of inches and the equal sign
+    int inches;                                  // A variable to store total inches 
+    inches = 120;                                // Setting the value for inches 
+    cout << inches << " inch(es) = " ;           //output the value of inches and the equal sign
     cout << inches / 12 << " feet (foot) and " ; // output the amount of feet(foot)
     cout << inches % 12 << " inch(es)" << endl;  // output remainding inches
 
 // Comparisons 
-    
+   cout << Jos <<  " < " << 'Q' << "\n";            //Show that the value of Q is higher than the value of "JOS"
+
 // Expressions 
-    cout << "2 + 3.5 = " << 2 + 3.5 << endl;
-    cout << " 6 / 4 + 3.9 = " << 6 / 4 +3.9 << endl;
-    cout << " 5.2*2 - (13.6 + 18/2) = " << 5.2*2 - (13.6 + 18/2) << endl;
+    cout << "2 + 3.5 = " << 2 + 3.5 << endl;                                 // simple mathematical expression
+    cout << " 6 / 4 + 3.9 = " << 6 / 4 +3.9 << endl;                         // semi complex mathematical expression
+    cout << " 5.2*2 - (13.6 + 18/2) = " << 5.2*2 - (13.6 + 18/2) << endl;    // complex mathemiatica expression 
     
     return 0;
 }
